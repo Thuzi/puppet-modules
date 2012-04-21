fc53d9e9703bcea1b98afee60995358eb5647803
gabrtv <gabriel@opdemand.com>
2012-04-21 14:18:02 -0600

    add opdemand::inputs::export

--- opdemand/manifests/inputs.pp
+++ opdemand/manifests/inputs.pp
@@ -1,11 +1,26 @@
 class opdemand::inputs {
 
-  file {"/var/cache/opdemand/inputs.json":
+  $exec_path = "/var/cache/opdemand"
+  $json_path = "$exec_path/inputs.json"
+  $env_path = "$exec_path/inputs.sh"
+  
+  file {$json_path:
     ensure => present
   }
 
-  file {"/var/cache/opdemand/inputs.sh":
+  file {$env_path:
     ensure => present
+  }
+  
+  exec {"export":
+    cwd = $opdemand::inputs::exec_path,
+    command => "/usr/local/bin/opdemand-export",
+    path => ["/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
+    user => "root",
+    group => "root",
+    require => File[$json_path],
+    # trigger on change to inputs.json
+    subscribe => File[$json_path],
   }
   
 }
