a259d2aad2ec5ec96736ddc3707b73d2b7d79df9
gabrtv <gabriel@opdemand.com>
2012-04-21 16:02:57 -0600

    cleanup rebuild-upstart for python

--- python/manifests/config.pp
+++ python/manifests/config.pp
@@ -1,13 +1,19 @@
 class python::config {
 
-  exec {"upstart":
+  # local variables
+  $repository_path = "$python::params::repository_path"
+  $app_name = "$python::params::app_name"
+  $username = "$python::params::username"
+  $env_path = "$opdemand::inputs::env_path"
+  
+  exec {"rebuild-upstart":
     path => ["/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
-    cwd => "$python::params::repository_path",
-    command => "sudo foreman export upstart /etc/init -a $python::params::app_name -u $python::params::username -e $env_inputs",
-    # re-exec on inputs.sh change
-    subscribe => File[$opdemand::inputs::env_path],
+    cwd => $repository_path,
+    command => "foreman export upstart /etc/init -a $app_name -u $username -e $env_path",
+    # rebuild on inputs.sh change
+    subscribe => File[$env_path],
     # notify the service on change
-    notify => Service[$python::params::app_name],
+    notify => Service[$app_name],
   }
 
 }
