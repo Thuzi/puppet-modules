e5895ad5321e806d72e273a7bc181b72788a7007
gabrtv <gabriel@opdemand.com>
2012-04-29 17:07:19 -0600

    first pass at new nginx module

---
+++ nginx/manifests/install.pp
@@ -0,0 +1,25 @@
+class nginx::install {
+
+  # local variables
+  $username = "$python::params::username"
+  $group = "$python::params::group"
+  $repository_path = "$python::params::repository_path"
+  $ppa = "ppa:nginx/stable" # use latest stable
+  $packages = [ "nginx" ]
+  $apache_packages = [ "apache2.2-common", "apache2.2-bin", "apache2-utils" ]
+  
+  # install ppa
+  apt::ppa {$nginx_ppa:}
+  
+  # install base python packages
+  package { $packages:
+    ensure => latest,
+    require => Apt::ppa[$ppa],
+  }
+
+  # remove apache2 packages that may be auto-installed on ubuntu
+  package { $apache_packages:
+    ensure => absent,
+  }
+  
+}
