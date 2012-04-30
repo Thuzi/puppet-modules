160c2cbb9737b7d529e50d89ac44cc06b5297aca
gabrtv <gabriel@opdemand.com>
2012-04-29 19:12:39 -0600

    deprecate foreman-level workers, we can do that in the Procfiles w/ gunicorn, etc

--- python/manifests/config.pp
+++ python/manifests/config.pp
@@ -11,7 +11,7 @@ class python::config {
   exec {"rebuild-upstart":
     path => ["/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
     cwd => $repository_path,
-    command => "foreman export upstart /etc/init -a $app_name -u $username -e $env_path -c web=$num_workers -t /var/cache/opdemand",
+    command => "foreman export upstart /etc/init -a $app_name -u $username -e $env_path -t /var/cache/opdemand",
     # rebuild on change of inputs.sh or the vcsrepo
     subscribe => [ File[$env_path], Vcsrepo[$repository_path] ],
     # notify the service on change
