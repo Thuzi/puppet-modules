fc261a5c85bce645807386f511d399ff354f67d9
gabrtv <gabriel@opdemand.com>
2012-05-03 14:07:57 -0600

    add opdemand::app::clojure

---
+++ opdemand/manifests/app/clojure.pp
@@ -0,0 +1,21 @@
+class opdemand::app::clojure {
+
+  # require opdemand common and repo
+  require opdemand::common
+  require opdemand::app::repository
+  
+  # initialize dynamic parameters
+  class {"clojure::params":
+    username => hiera("application/username", "ubuntu"),
+    group => hiera("application/group", "ubuntu"),
+    home => hiera("application/home", "/home/ubuntu"),
+    repository_path => hiera("application/repository_path", "/home/ubuntu/repo"),
+    app_name => hiera("application/name", "clojure"),
+  }
+
+  # include relevant python classes
+  include clojure::install
+  include clojure::config
+  include clojure::service
+
+}
