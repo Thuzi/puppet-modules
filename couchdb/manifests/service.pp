e2513b392f09b2281d82012e048393a6f950234d
Matt Boersma <matt@opdemand.com>
2012-05-18 11:38:39 -0600

    Trying to fix the couchdb chicken-egg issue again.

--- couchdb/manifests/service.pp
+++ couchdb/manifests/service.pp
@@ -1,9 +1,9 @@
 class couchdb::service {
-  
+
   service {"couchdb":
+    enable    => true,
     ensure    => running,
     hasstatus => true,
-    enable    => true,
     require   => [Package["couchdb"], File["/etc/couchdb/local.ini"]],
     subscribe => File["/etc/couchdb/local.ini"],
   }
