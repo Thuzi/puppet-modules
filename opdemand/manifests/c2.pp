ded14397c94d67871403f85bee8aedeefb3231c2
gabrtv <gabriel@opdemand.com>
2012-11-20 15:23:41 -0700

    first pass at opdemand::c2

---
+++ opdemand/manifests/c2.pp
@@ -0,0 +1,18 @@
+class opdemand::c2 {
+
+  require opdemand::common
+
+  $username = hiera("APPLICATION_USERNAME", "ubuntu")
+  $group = hiera("APPLICATION_GROUP", "ubuntu")
+  $home = hiera("APPLICATION_HOME", "/home/ubuntu")
+  $repository_path = hiera("APPLICATION_REPOSITORY_PATH", "/home/ubuntu/repo")
+  $app_name = hiera("APPLICATION_NAME", "python")
+      
+  class {"c2::install":} ->
+  class {"c2::config":} ->
+  class {"c2::repository":} ->
+  class {"c2::server":} ->
+  class {"c2::proxy":} ->
+  class {"c2::service":}
+
+}
