b91361c67339d92c3233fe272000bb1d97bc0c8d
gabrtv <gabriel@opdemand.com>
2012-12-07 14:56:24 -0700

    log output of replica set init

--- mongodb/manifests/service.pp
+++ mongodb/manifests/service.pp
@@ -14,6 +14,7 @@ class mongodb::service (
   if $replSet {
     # replication config
     exec { "rs.initiate":
+      logoutput => true,
       command => 'mongo --eval "printjson(rs.initiate())"',
       cwd => "/root",
       path => ["/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
