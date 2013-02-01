2793f9c7a554b9fa0c51a848f6d9e3da5015e10c
Gabriel Monroy <gabriel@opdemand.com>
2013-02-01 11:21:40 -0700

    fix syntax error

--- opdemand/manifests/mq/rabbitmq.pp
+++ opdemand/manifests/mq/rabbitmq.pp
@@ -2,7 +2,7 @@ class opdemand::mq::rabbitmq {
 
   require opdemand::common
 
-  class {"rabbitmq::config"  
+  class {"rabbitmq::config": 
     bind => hiera("RABBITMQ_SERVER_BIND", ""), # defaults to all interfaces
     port => hiera("RABBITMQ_SERVER_PORT", 5672),
     nodename => hiera("RABBITMQ_SERVER_NODENAME", $hostname),
