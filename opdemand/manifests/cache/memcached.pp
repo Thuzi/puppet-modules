871b20f7a2691faa0468a4892b3ae6169d31b2ff
Gabriel Monroy <gabriel@opdemand.com>
2013-02-01 11:50:30 -0700

    use clearer variable names

--- opdemand/manifests/cache/memcached.pp
+++ opdemand/manifests/cache/memcached.pp
@@ -3,10 +3,10 @@ class opdemand::cache::memcached {
   require opdemand::common
 
   class {"memcached::params":
-    bind => hiera("MEMCACHED_SERVER_BIND", "0.0.0.0"),
+    memory => hiera("MEMCACHED_SERVER_MEMORY", "256"), # in megabytes
     port => hiera("MEMCACHED_SERVER_PORT", "11211"),
-    size => hiera("MEMCACHED_SERVER_MEMORY", "256"),  # in megabytes
-    username => hiera("MEMCACHED_USERNAME", "memcache"), # for daemon    
+    bind => hiera("MEMCACHED_SERVER_BIND", "127.0.0.1"),
+    daemon_user => hiera("MEMCACHED_SERVER_DAEMONUSER", "memcache"), # for daemon    
   }
   
   include memcached::install
